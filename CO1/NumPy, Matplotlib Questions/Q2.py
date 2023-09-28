5b584426fff68ac0f53f445a0c30cee8e7087622
Alanthomas-2209 <2209@tkmce.ac.in>
2023-09-28 18:43:16 +0530

    Q2 almost completed

--- CO1/NumPy, Matplotlib Questions/Q2.py
+++ CO1/NumPy, Matplotlib Questions/Q2.py
@@ -1,9 +1,39 @@
+# import numpy as np
+
+# rows = int(input("Enter the number of rows: "))
+# cols = int(input("Enter the number of columns: "))
+
+# Arr = np.empty((rows, cols), dtype=int)
+
+# print("Enter the elements of the array row by row:")
+
+# for i in range(rows):
+#     row = input().split()
+#     if len(row) != cols:
+#         print("Error: Each row should have", cols, "elements.")
+#         exit(1)
+#     Arr[i] = [int(x) for x in row]
+
+# seq_str = input("Enter the sequence to find separated by spaces: ")
+# seq = np.array([int(x) for x in seq_str.split()])
+
+# occurrences = 0
+
+# for i in range(rows):
+#     for j in range(cols):
+#         if i + len(seq) <= rows and j + len(seq) <= cols:
+#             subarray = Arr[i:i+len(seq), j:j+len(seq)]
+#             if np.array_equal(subarray.flatten(), seq):
+#                 occurrences += 1
+
+# print("Number of occurrences:", occurrences)
+
 import numpy as np
 
 rows = int(input("Enter the number of rows: "))
 cols = int(input("Enter the number of columns: "))
 
-Arr = np.empty((rows, cols), dtype=int)
+arr = np.empty((rows, cols), dtype=int)
 
 print("Enter the elements of the array row by row:")
 
@@ -12,18 +42,24 @@ for i in range(rows):
     if len(row) != cols:
         print("Error: Each row should have", cols, "elements.")
         exit(1)
-    Arr[i] = [int(x) for x in row]
+    arr[i] = [int(x) for x in row]
 
 seq_str = input("Enter the sequence to find separated by spaces: ")
 seq = np.array([int(x) for x in seq_str.split()])
 
-occurrences = 0
+arr_1d = arr.ravel()
+seq_1d = seq.ravel()
 
-for i in range(rows):
-    for j in range(cols):
-        if i + len(seq) <= rows and j + len(seq) <= cols:
-            subarray = Arr[i:i+len(seq), j:j+len(seq)]
-            if np.array_equal(subarray.flatten(), seq):
-                occurrences += 1
+arr_1d = arr_1d.tolist()
+seq_1d = seq_1d.tolist()
+count = 0
+for i in range(0, len(arr_1d) - len(seq_1d) + 1):
+    if arr_1d[i:i+len(seq_1d)] == seq_1d:
+        count += 1 
+print('Solution1:', count)
 
-print("Number of occurrences:", occurrences)
+# arr_1d = arr.ravel()
+# seq_1d = np.array([-9, 4])  
+# res = np.convolve(arr_1d, seq_1d)
+
+# print('Solution2:', res.size - np.count_nonzero(res))
